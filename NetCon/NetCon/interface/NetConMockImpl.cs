1973f60a62ffd6ebb73c82df3a170fd9cfbb9adc
Adam Puchała <adam.puchala@outlook.com>
2020-11-15 21:28:54 +0100

    implemented pcap file frames loading.

--- NetCon/NetCon/interface/NetConMockImpl.cs
+++ NetCon/NetCon/interface/NetConMockImpl.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -14,22 +15,7 @@ namespace NetCon.inter
         private Boolean shouldWork;
         private Boolean captureState;
 
-        private Byte[] mockData = {
-                            0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0xd5, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00, 0x01,
-                            0x05, 0x1b, 0x9f, 0x96, 0x08, 0x00, 0x45, 0x00, 0x00, 0xd0, 0x4a, 0x0d, 0x00, 0x00, 0x80, 0x11,
-                            0x78, 0x75, 0xa9, 0xfe, 0x23, 0x9e, 0xa9, 0xfe, 0xff, 0xff, 0x00, 0x8a, 0x00, 0x8a, 0x00, 0xbc,
-                            0x22, 0xf4, 0x11, 0x0e, 0xe7, 0xb1, 0xa9, 0xfe, 0x23, 0x9e, 0x00, 0x8a, 0x00, 0xa6, 0x00, 0x00,
-                            0x20, 0x45, 0x43, 0x45, 0x46, 0x45, 0x44, 0x45, 0x4c, 0x46, 0x50, 0x45, 0x44, 0x46, 0x49, 0x46, 
-                            0x50, 0x45, 0x48, 0x43, 0x41, 0x43, 0x41, 0x43, 0x41, 0x43, 0x41, 0x43, 0x41, 0x43, 0x41, 0x41, 
-                            0x41, 0x00, 0x20, 0x46, 0x44, 0x46, 0x45, 0x45, 0x46, 0x46, 0x43, 0x45, 0x50, 0x46, 0x48, 0x45, 
-                            0x4f, 0x45, 0x4a, 0x45, 0x4c, 0x45, 0x4a, 0x43, 0x41, 0x43, 0x41, 0x43, 0x41, 0x43, 0x41, 0x43, 
-                            0x41, 0x42, 0x4e, 0x00, 0xff, 0x53, 0x4d, 0x42, 0x25, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-                            0x00, 0x00, 0x00, 0x00, 0x11, 0x00, 0x00, 0x0c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-                            0x00, 0xe8, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0c, 0x00, 0x56, 0x00, 0x03, 
-                            0x00, 0x01, 0x00, 0x01, 0x00, 0x02, 0x00, 0x1d, 0x00, 0x5c, 0x4d, 0x41, 0x49, 0x4c, 0x53, 0x4c, 
-                            0x4f, 0x54, 0x5c, 0x42, 0x52, 0x4f, 0x57, 0x53, 0x45, 0x00, 0x02, 0x00, 0x42, 0x45, 0x43, 0x4b, 
-                            0x5f, 0x43, 0x58, 0x5f, 0x47, 0x00, 0x41, 0x62, 0x8f, 0xe0};
+        List<Byte[]> frames = new List<Byte[]>();
 
         private IntPtr mockDataPtr;
         public void sendAndReceiveMdio() { }
@@ -45,16 +31,22 @@ namespace NetCon.inter
 
         public void startCapture()
         {
+            loadData();
             shouldWork = true;
-            while (shouldWork)
+            foreach(var frame in frames)
             {
-                mockDataPtr = Marshal.AllocHGlobal(mockData.Length);
-                Marshal.Copy(mockData, 0, mockDataPtr, mockData.Length);
+                if (!shouldWork)
+                {
+                    break;
+                }
+
+                mockDataPtr = Marshal.AllocHGlobal(frame.Length);   //memory leak !
+                Marshal.Copy(frame, 0, mockDataPtr, frame.Length);
                 if (captureState)
                 {
-                    frameListener(mockDataPtr, mockData.Length);
+                    frameListener(mockDataPtr, frame.Length);
                 }
-                Thread.Sleep(1000);
+                Thread.Sleep(10);
             }
         }
 
@@ -62,6 +54,59 @@ namespace NetCon.inter
         {
             shouldWork = false;
             Marshal.FreeHGlobal(mockDataPtr);
+        }
+
+        private void loadData()
+        {
+            Byte[] data = File.ReadAllBytes("./mock.pcap");
+            List<Byte> frame = new List<Byte>();
+            pushFrameHeader(frame);
+            for (int i = 25; i < data.Length; i++){
+                if(data[i] == 0x55)
+                {
+                    Byte[] headerCandidate = new Byte[8];
+                    Array.ConstrainedCopy(data, i-1, headerCandidate, 0, 8);
+                    if (checkForHeader(headerCandidate))
+                    {
+                        frames.Add(frame.ToArray());
+                        frame = new List<Byte>();
+                        pushFrameHeader(frame);
+                        i += 7;
+                        continue;
+                    }
+                    else
+                    {
+                        frame.Add(data[i]);
+                    }
+                }
+                else
+                {
+                    frame.Add(data[i]);
+                }
+            }
+        }
+
+        private void pushFrameHeader(List<Byte> frame)
+        {
+            frame.Add(0x55);
+            frame.Add(0x55);
+            frame.Add(0x55);
+            frame.Add(0x55);
+            frame.Add(0x55);
+            frame.Add(0x55);
+            frame.Add(0x55);
+            frame.Add(0xd5);
+        }
+
+        private bool checkForHeader(Byte[] headerCandidate)
+        {
+            bool isHeader = true;
+            for (int j = 0; j < 7; j++)
+            {
+                isHeader &= headerCandidate[j] == 0x55;
+            }
+            isHeader &= headerCandidate[7] == 0xd5;
+            return isHeader;
         }
     }
 }
